a8b9b74f1926aff22e1e86c0106163e963e2d8c5
Richard Hess <eswat2@gmail.com>
2018-01-23 10:41:46 -0800

    add link to repo homepage, if defined

--- src/components/Github/Repos.vue
+++ src/components/Github/Repos.vue
@@ -5,9 +5,15 @@
     <li class="list-group-item"
         :key="indx"
         v-for="(repo, indx) in repos">
-      <h4 v-if="repo.html_url"><a :href="repo.html_url">{{repo.name}}</a></h4>
+      <h4 v-if="repo.html_url">
+        <a :href="repo.html_url" target="_blank" >{{repo.name}}</a>
+        <a :href="repo.homepage" target="_blank" v-if="hasValue(repo.homepage)">
+          <i class="fa fa-home my-site"></i>
+        </a>
+      </h4>
       <h4 v-else>{{ repo.name }}></h4>
-      <p v-if="repo.description">{{repo.description}}</p>
+      <p v-if="repo.description"
+         class="my-grey">{{repo.description}}</p>
     </li>
   </ul>
 </div>
@@ -15,10 +21,26 @@
 
 <script>
 export default {
-  props: [ 'repos' ]
+  props: [ 'repos' ],
+  methods: {
+    hasValue( val ) {
+      return ( val !== null ) && ( val !== '' )
+    }
+  }
 }
 </script>
 
 <style scoped>
+.my-grey {
+  color: grey;
+}
 
+.my-site {
+  margin-left: 5px;
+  cursor: pointer;
+}
+
+.my-site:hover {
+  color: red;
+}
 </style>
